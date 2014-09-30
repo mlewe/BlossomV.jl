9678c7eeac1878474b3b408f97a1f848972f9024
Marius Lewerenz <marius.lewerenz@gmail.com>
2014-09-30 16:40:42 +0200

    Add interface with c calling conventions.

--- deps/build.jl
+++ deps/build.jl
@@ -4,7 +4,7 @@ using BinDeps
 
 blossom5 = library_dependency("blossom5.so")
 
-provides(Sources, URI("http://pub.ist.ac.at/~vnk/software/blossom5-v2.04.src.tar.gz"), blossom5)
+provides(Sources, URI("http://pub.ist.ac.at/~vnk/software/blossom5-v2.04.src.tar.gz"), blossom5, SHA="22ec3d6ac23fe8adcce23393d08473af46bf9427105540a7e36f6807c0b50c0c")
 
 prefix = usrdir(blossom5)
 blossom5srcdir = joinpath(srcdir(blossom5), "blossom5-v2.04.src")
@@ -18,10 +18,11 @@ provides(BuildProcess,
                 @build_steps begin
                     CreateDirectory(joinpath(prefix, "lib"))
                     `sh -c "patch < ../../sharedlib.patch"`
+                    `sh -c "cp ../../interface/* ./"`
                     MakeTargets(["all"])
                     `cp blossom5.so $prefix/lib/blossom5.so`
                 end)
         end
     end), blossom5)
 
-@BinDeps.install [:blossom5  => :_jl_libblossm5]
+@BinDeps.install [:blossom5  => :_jl_libblossom5]
---
+++ deps/interface/interface.cpp
@@ -0,0 +1,47 @@
+
+#include "interface.h"
+
+#include "PerfectMatching.h"
+
+#include <memory>
+
+struct matching {
+    template<typename... Args>
+    matching(Args&&... args)
+    : actual(std::forward<Args>(args)...)
+    {
+    }
+
+    PerfectMatching actual;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+matching_t matching_construct(int32_t node_num, int32_t edge_num_max) {
+    matching_t result = nullptr;
+    result = std::make_unique<matching>(node_num, edge_num_max).release();
+    return result;
+}
+
+void matching_destruct(matching_t matching) {
+    delete matching;
+}
+
+int32_t matching_add_edge(matching_t matching, int32_t first_node, int32_t second_node, int32_t cost) {
+    matching->actual.AddEdge(first_node, second_node, cost);
+}
+
+void matching_solve(matching_t matching) {
+    matching->actual.Solve();
+}
+
+int32_t matching_get_match(matching_t matching, int32_t node) {
+    return matching->actual.GetMatch(node);
+}
+
+#ifdef __cplusplus
+}
+#endif
